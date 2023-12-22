425f2bde1689539e186e709f425b37d7f192e8f9
merima <merima.maksumic@stu.ibu.edu.ba>
2023-12-22 01:18:24 +0100

    second

---
+++ core/page-objects/loginpage.ts
@@ -0,0 +1,58 @@
+import { By, WebDriver, until } from "selenium-webdriver";
+import BasePage from "./base-page";
+import { readFileSync } from "fs";
+import * as path from "path";
+
+let basePage: BasePage;
+let testData: any; // Change the type based on the structure of your JSON data
+
+const dataFilePath = path.resolve(__dirname, "../data/data.json");
+
+try {
+  const jsonData = readFileSync(dataFilePath, "utf8");
+  testData = JSON.parse(jsonData);
+} catch (error) {
+  console.error('Error parsing JSON:', error);
+  // Handle the error gracefully, log it, or throw it further.
+}
+
+
+
+export class LoginPage extends BasePage {
+    constructor(driver: WebDriver) {
+        super(driver);
+    }
+
+    private userBtn: By = By.xpath("//div [@class='nav-item']//*[@href='https://buybook.ba/moj-nalog']");
+
+
+    async clickUserButton(){
+        await this.findElementAndClick(this.userBtn);
+    }
+   
+  private email: By = By.xpath("//input [@id='email_1']");
+
+  async provideEmail(){
+        await this.fillInputField(this.email, testData.data.email);
+    }
+
+
+    private password: By = By.xpath("//input [@id='exampleInputPassword01']");
+    async providePassword(){
+        await this.fillInputField(this.password, testData.data.password);
+    }
+
+    
+
+    private SumbitBtn: By = By.xpath("//button[@name='giris']");
+    async clickSumbitBtn(){
+        await this.findElementAndClick(this.SumbitBtn);
+    }
+    private userBtnagain: By = By.xpath("//div [@class='nav-item']//*[@href='https://buybook.ba/moj-nalog']");
+
+
+    async clickUserButtonagain(){
+        await this.findElementAndClick(this.userBtnagain);
+    }
+   
+}
---
+++ tests/test2.test.ts
@@ -0,0 +1,34 @@
+import { Builder } from "selenium-webdriver";
+import { LoginPage } from "../core/page-objects/loginpage";
+
+let driver;
+let url = "https://buybook.ba/";
+
+// Declare the variable
+let loginPage: LoginPage;
+
+beforeAll(async () => {
+    driver = await new Builder().forBrowser("chrome").build();
+    await driver.get(url);
+    await driver.manage().window().maximize();
+  }, 600000);
+  
+test("loginpage: user login", async () => 
+{
+    await driver.navigate().to(url);
+    await driver.manage().setTimeouts({ implicit: 15000000 });
+    loginPage = new LoginPage(driver); 
+    await loginPage.clickUserButton(); 
+    await loginPage.provideEmail();
+    await loginPage.providePassword();
+    await loginPage.clickSumbitBtn();
+    await loginPage.clickUserButtonagain();
+}, 
+1000000);
+  
+  
+  afterAll(async () => {
+  
+    await driver.quit();
+  
+  }, 50000000);
